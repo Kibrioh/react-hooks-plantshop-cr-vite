6307cd5bdbc2ae5e2263eced908e6284aafcb678
Brian Kiprotich <briankiprotich602@gmail.com>
2026-06-07 12:02:18 +0300

    Plant forms

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((plants) => setPlants(plants));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants((plants) => [...plants, newPlant]);
+  }
+
+  function handleSearchChange(searchTerm) {
+    setSearchTerm(searchTerm);
+  }
+
+  const displayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={handleSearchChange} />
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
